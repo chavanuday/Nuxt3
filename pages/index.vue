a107eda6e7b491b24fd119d894d1fca8f4e5e7e1
Uday Chavan Git <udaymchavan228@gmail.com>
2023-03-28 00:06:07 +0530

    second commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi incidunt
+      consequatur minima deserunt nisi id minus exercitationem expedita impedit
+      laudantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi incidunt
+      consequatur minima deserunt nisi id minus exercitationem expedita impedit
+      laudantium!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
